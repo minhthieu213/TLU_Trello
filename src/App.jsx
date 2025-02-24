05d6fe6be13446a589f70eb11d834982e67738dd
minhthieu213 <minhthieu213@gmail.com>
2025-02-24 19:35:55 +0700

    charge: conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Button from "@mui/material/Button";
-import AbcIcon from '@mui/icons-material/Abc';
+import AbcIcon from "@mui/icons-material/Abc";
 
 function App() {
   return (
     <>
-      <p>Minh</p>
+      <div>Minh</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
